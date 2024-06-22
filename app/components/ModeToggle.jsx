23cb96539fe8dee92488912fa25e2a248b7bd519
shirshen <official.shirshen@gmail.com>
2024-06-23 02:19:14 +0600

    feat: theming completed for mobile also requesting pull request

---
+++ app/components/ModeToggle.jsx
@@ -0,0 +1,39 @@
+"use client";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import React from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { MdSunny } from "react-icons/md";
+
+export default function ModeToggle() {
+    const { theme, setTheme } = useTheme();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="text-2xl">
+                    {theme == "dark" ? <BsFillMoonStarsFill /> : <MdSunny />}
+
+                    <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
