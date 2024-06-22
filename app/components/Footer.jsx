c830c950f95cf5058241f2ce1db8f0ac74218f43
shirshen <official.shirshen@gmail.com>
2024-06-23 03:25:34 +0600

    feat: Up and down arrow for navigation

---
+++ app/components/Footer.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleUp } from "react-icons/fa6";
+
+export default function Footer() {
+    const scrollUp = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    const scrollDown = () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth",
+        });
+    };
+    return (
+        <div className="fixed bottom-6 right-6 flex flex-col z-[1000] gap-3">
+            <Button
+                type="button"
+                size="icon"
+                className="ml-4 opacity-25 hover:opacity-70"
+                onClick={scrollUp}>
+                <FaAngleUp />
+                <span className="sr-only">Go up</span>
+            </Button>
+            <Button
+                type="button"
+                size="icon"
+                className="ml-4 opacity-25 hover:opacity-70"
+                onClick={scrollDown}>
+                <FaAngleDown />
+                <span className="sr-only">Go up</span>
+            </Button>
+        </div>
+    );
+}
